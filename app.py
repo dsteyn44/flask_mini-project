9129cae6e5afb8a324dda9a80791f9cfbca5072a
dsteyn44 <d_steyn@yahoo.com>
2021-07-17 10:58:40 +0000

    Add import PyMongo, import ObjectID, MONGO_URI config

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 @app.route("/")
 def hello():
     return "Hello World ... again!"
-    
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
